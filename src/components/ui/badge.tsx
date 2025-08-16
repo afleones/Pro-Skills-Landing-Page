fb86e3a0634a61aca62a74aacd210ab60f6e6273
afleones <afleones@icloud.com>
2025-08-16 17:43:35 -0500

    Landing page ready

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Slot } from "@radix-ui/react-Slot";
+import { slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@utils/twMerge";
@@ -32,7 +32,7 @@ function Badge({
   ...props
 }: React.ComponentProps<"span"> &
   VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
-  const Comp = asChild ? Slot : "span";
+  const Comp = asChild ? slot : "span";
 
   return (
     <Comp
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Slot } from "@radix-ui/react-Slot";
+import { slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@utils/twMerge";
@@ -44,7 +44,7 @@ function Button({
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean;
   }) {
-  const Comp = asChild ? Slot : "button";
+  const Comp = asChild ? slot : "button";
 
   return (
     <Comp
